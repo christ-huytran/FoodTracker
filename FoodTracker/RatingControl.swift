9587a8151ea118324f90b1a47a96f9def733bbb9
Chris Tran <christopher.huytran@gmail.com>
2015-11-16 12:12:48 -0800

    Add buttons for rating

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,45 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Huy Tran on 11/16/15.
+//  Copyright © 2015 Chris Tran. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    // MARK: Properties
+    var rating = 0
+    var ratingButtons = [UIButton]()
+    
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        for _ in 0..<5 {
+            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+            button.backgroundColor = UIColor.redColor()
+            button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
+            ratingButtons += [button]
+            addSubview(button)
+        }
+    }
+    
+    override func intrinsicContentSize() -> CGSize {
+        return CGSize(width: 240, height: 44)
+    }
+    
+    // MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
+}
