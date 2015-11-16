98204cbc3774b604f59487f2437e7ffb528a884b
Chris Tran <christopher.huytran@gmail.com>
2015-11-16 14:38:33 -0800

    Add unit testing to data model

---
+++ Meal.swift
@@ -0,0 +1,28 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Huy Tran on 11/16/15.
+//  Copyright © 2015 Chris Tran. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    // MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+}
